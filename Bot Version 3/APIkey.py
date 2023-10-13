e0813a3e9342b8a2e7ac38d649024826291e37ed
cdmill <77467167+CaelenMiller@users.noreply.github.com>
2023-10-13 14:26:48 -0600

    Update APIkey.py

--- Bot Version 3/APIkey.py
+++ Bot Version 3/APIkey.py
@@ -1,4 +1,4 @@
 import openai
 
-openai.organization = "org-B3fzX9iYhcbGE1ikotEgdsy6"
-openai.api_key = "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"
+openai.organization = "Put your org ID here"
+openai.api_key = "Put your key here"
